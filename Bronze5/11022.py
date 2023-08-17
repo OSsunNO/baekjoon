165e7529c4bb5c79a0cac1764dc3bef4b9fb3d8e
Sunho <1020sunho@naver.com>
2023-08-17 15:21:24 +0900

    11022 문제 재풀이 완료

---
+++ Bronze5/11022.py
@@ -0,0 +1,6 @@
+for i in range(1, int(input())+1):
+    a, b = map(int,input().split())
+    print("Case #{}: {} + {} =".format(i,a,b),a+b)
+
+# for i in range(1, int(input())+1):
+#     a,b = map(int,input().split());print(f"Case #{i}: {a} + {b} =",a+b)
