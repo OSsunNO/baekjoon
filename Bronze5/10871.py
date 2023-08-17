a83b82545106da42590761958149d4e380143a1a
Sunho <1020sunho@naver.com>
2023-08-17 14:00:00 +0900

    10871 문제 재풀이 및 수정 완료

--- Bronze5/10871.py
+++ Bronze5/10871.py
@@ -1,7 +1,7 @@
 n, x = map(int, input().split())
 arr = []
+num = [*input().split()] # input 받자마자 공백을 기준으로 요소 받아서 리스트 생성
 for i in range(n):
-    num = int(input())
-    if x > num:
-        arr.append(num)
+    if x > int(num[i]):
+        arr.append(num[i])
 print(*arr) # * (배열도 asterisk로 압축해제가능) 굳이 for 문으로 출력할 필요 x
