9be30c8a08fc0d6cb326fe4b7ce589abd45fdc0c
Sunho <1020sunho@naver.com>
2023-08-22 23:08:28 +0900

    2562 풀이 완료 - 다른 풀이 존재 다른 풀이 - for문의 특징을 이용해 for 구문 한 줄로 답을 구함 print(*max(int(input()),i+1) for i in range(9)) 이런 구조로 코드를 짜면 max()안에서 for문이 반복되면서 9번 입력받은 값 중 최댓값과 그 인덱스 값을 튜플 형태로 가지고 그걸 unpack해서 출력

---
+++ bronze3/2562.py
@@ -0,0 +1,14 @@
+# m = 0
+# n = 0
+# for i in range(9):
+#     k = int(input())
+#     if(k>m):
+#         m = k
+#         n = i+1
+# print(m,n,sep='\n')
+
+# 다른풀이
+print(max((int(input()),i+1) for i in range(9)))
+
+
+
